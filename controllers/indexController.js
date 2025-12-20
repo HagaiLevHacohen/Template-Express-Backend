b123cc2cb6fc4ff72d9eb912522e415efb8af4ff
HagaiLevHacohen <levhacohen@mail.tau.ac.il>
2025-12-20 18:41:08 +0200

    updated a bit the validation part (added an import)

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,7 @@
 // controllers/indexController.js
 
 const db = require("../db/queries");
+const { body, validationResult, matchedData } = require("express-validator");
 const CustomNotFoundError = require("../errors/CustomNotFoundError");
 
 const getIndex = async (req, res) => {
